cdade52a958aeb2dcd77c7daceabcf3125525d58
H3wastooshort <hacker3000@posteo.org>
2024-02-08 00:11:48 +0100

    BOM thing

---
+++ BikeUSBv2/jlcpcb/bom_convert.py
@@ -0,0 +1,18 @@
+import csv
+count=0
+with open("production_files/BOM-BikeUSBv2.csv",'r') as f:
+    reader=csv.reader(f)
+    with open("production_files/BOM-BikeUSBv2_lcsc.csv",'w') as fo:
+        writer=csv.writer(fo)
+        for r in reader:
+            n=r[1].count(',')+1
+            n*=1#how many should there be parts for?
+            ro=[r[0],r[3],n]
+            writer.writerow(ro)
+            #print(r[1]+" -> "+str(n))
+            print(ro)
+            count += n
+
+print()
+print("Component Count: " + str(count))
+
