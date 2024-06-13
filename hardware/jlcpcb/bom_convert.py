6cd51a9b9080f29bb21a090b9f3d6f6589d46e4e
H3wastooshort <hacker3000@posteo.org>
2024-06-13 23:58:48 +0200

    fix stuff like thermals for U5

--- hardware/jlcpcb/bom_convert.py
+++ hardware/jlcpcb/bom_convert.py
@@ -6,7 +6,7 @@ with open("production_files/BOM-BikeUSBv2.csv",'r') as f:
         writer=csv.writer(fo)
         for r in reader:
             n=r[1].count(',')+1
-            n*=1#how many should there be parts for?
+            n*=3#how many should there be parts for?
             ro=[r[0],r[3],n]
             writer.writerow(ro)
             #print(r[1]+" -> "+str(n))
