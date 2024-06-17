d45aeee6b47b7faeee65c386a7a1e1bd27621039
H3wastooshort <hacker3000@posteo.org>
2024-06-17 16:15:37 +0200

    eeprom check might work now

--- firmware/BikeUSBv2/eeprom_util.h
+++ firmware/BikeUSBv2/eeprom_util.h
@@ -1,21 +1,24 @@
 //EEPROM Integrity Check
+#include "lib/crc32.h"
 
-#define CRC_POS EEPROM.length() - 1 - sizeof(crc)
+#define CRC_POS ((EEPROM.length() - 1) - sizeof(crc))
 
 bool EEPROM_checkIntegrity() {
+  uint32_t table[256];
+  crc32::generate_table(table);
+
   uint32_t crc;
   size_t pos = CRC_POS;
   EEPROM.get(pos, crc);
-  for (size_t i = 0; i < pos; i++) {
-    
-  }
+  for (size_t i = 0; i < pos; i++) crc = crc32::update(table, crc, EEPROM.read(i));
 }
 
 void EEPROM_markClean() {
+  uint32_t table[256];
+  crc32::generate_table(table);
+
   uint32_t crc;
   size_t pos = CRC_POS;
-  for (size_t i = 0; i < pos; i++) {
-
-  }
+  for (size_t i = 0; i < pos; i++) crc = crc32::update(table, crc, EEPROM.read(i));
   EEPROM.put(pos, crc);
 }
