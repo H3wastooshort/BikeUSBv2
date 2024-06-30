379f57061f09e76508ddcaa57885f82b228a5ae6
H3wastooshort <hacker3000@posteo.org>
2024-07-01 00:28:40 +0200

    src stuff

--- firmware/BikeUSBv2/pd_src.2.h
+++ firmware/BikeUSBv2/pd_src.2.h
@@ -7,6 +7,35 @@ void do_pd_msg_resp(uint8_t* msg, size_t len) {
       break;
   }
 }
+void send_source_cap() {
+  union {
+    uint32_t pdo;
+    uint8_t buf[4];
+  };
+
+  switch (msm_state) {
+    default:
+      return;
+      break;
+
+    case MSM_SWITCH_2W5:
+    case MSM_2W5:
+      pdo = PDStack_SRC::make_fixed_pdo(5000, 500);
+      break;
+
+    case MSM_SWITCH_5W:
+    case MSM_5W:
+      pdo = PDStack_SRC::make_fixed_pdo(5000, 1000);
+      break;
+
+    case MSM_SWITCH_7W5:
+    case MSM_7W5:
+      pdo = PDStack_SRC::make_fixed_pdo(5000, 1500);
+      break;
+  }  //
+  printDebug(DBG_PDO, pdo);
+  pd.send_data_msg(PDM_Source_Capabilities, buf, sizeof(buf));
+}
 
 enum pd_src_state_t {
   SRC_OFF,           //do nothing
@@ -31,35 +60,7 @@ void run_pd_src_sm() {
     case SRC_ADVERTIZE:
       {
         static uint32_t last_adv = 0;
-        if (millis() - last_adv > 100) {
-          union {
-            uint32_t pdo;
-            uint8_t buf[4];
-          };
-
-          switch (msm_state) {
-            default:
-              return;
-              break;
-
-            case MSM_SWITCH_2W5:
-            case MSM_2W5:
-              pdo = PDStack_SRC::make_fixed_pdo(5000, 500);
-              break;
-
-            case MSM_SWITCH_5W:
-            case MSM_5W:
-              pdo = PDStack_SRC::make_fixed_pdo(5000, 1000);
-              break;
-
-            case MSM_SWITCH_7W5:
-            case MSM_7W5:
-              pdo = PDStack_SRC::make_fixed_pdo(5000, 1500);
-              break;
-          }  //
-          printDebug(DBG_PDO, pdo);
-          pd.send_data_msg(PDM_Source_Capabilities, buf, sizeof(buf));
-        }
+        if (millis() - last_adv > 100) send_source_cap();
       }
       break;
 
@@ -80,24 +81,35 @@ void run_pd_src_sm() {
   }
 }
 
-void fusb_int() {
-  uint32_t i = fusb.get_interrupts();
-  if (i & FUSB_I_COMP_CHNG) {  //on at/detach
-    if (src_state == SRC_DETACHED) src_state = SRC_ADVERTIZE;
-    else src_state = SRC_DETACHED;  //TODO this is very bad! check with find_cc() if device is really gone
-  }
-}
-
 void on_message(uint8_t* msg, size_t len) {
   if (PDStack::is_data_msg(msg, len)) switch (PDStack::get_data_msg_type(msg, len)) {
+      case PDM_Request:
+        pd.send_ctrl_msg(PDM_Accept);  //not checking lol (at least for now)
+        break;
       default: pd.do_other_msg_resp(msg, len); break;
     }
 
   else switch (PDStack::get_ctrl_msg_type(msg, len)) {
+      case PDM_Get_Source_Cap: send_source_cap(); break;
       case PDM_GoodCRC:
         if (src_state == SRC_WAIT) src_state = SRC_STARTING_PSU;
         break;
       default: pd.do_other_msg_resp(msg, len); break;
+    }
+}
+
+void fusb_int() {
+  uint32_t interrupts = fusb.get_interrupts();
+  if (interrupts & FUSB_I_COMP_CHNG) {  //on at/detach
+    if (src_state == SRC_DETACHED) src_state = SRC_ADVERTIZE;
+    else src_state = SRC_DETACHED;  //TODO this is very bad! check with find_cc() if device is really gone
+  }
+
+  if (interrupts & FUSB_I_ACTIVITY)
+    if (fusb.rxb_state() == 0) { //TODO: this does not seem right
+      uint8_t buf[256];
+      pd.read_msg(buf, sizeof(buf));
+      on_message(buf, sizeof(buf));
     }
 }
 
