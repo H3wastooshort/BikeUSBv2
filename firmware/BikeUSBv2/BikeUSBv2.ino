c99cfcac4402f8ccdf8a25f426e3dab792760c7d
H3wastooshort <hacker3000@posteo.org>
2024-07-14 18:45:21 +0200

    calc freq usinsing micros()

--- firmware/BikeUSBv2/BikeUSBv2.ino
+++ firmware/BikeUSBv2/BikeUSBv2.ino
@@ -1,10 +1,10 @@
 /*
 megaTinyCore Board Config:
 BOD Mode = Active / Sampled (125Hz)
-BOD Voltage Level = 1.8V
+BOD Voltage Level = 2.6V
 Chip = ATtiny 1616
-Clock = 4Mhz Internal 
-millis()/micros() = Enabled (default timer) or maybe RTC (no micros). not sure yet
+Clock = 8Mhz Internal 
+millis()/micros() = Enabled (default timer)
 printf() = Minimal
 Startup Time = 64ms
 Programmer: SerialUPDI - SLOW
