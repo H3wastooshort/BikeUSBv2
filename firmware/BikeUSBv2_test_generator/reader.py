eadce59815865e2fe7637c775913d24e57cead6a
H3wastooshort <hacker3000@posteo.org>
2024-07-22 19:19:40 +0200

    avg values

--- firmware/BikeUSBv2_test_generator/reader.py
+++ firmware/BikeUSBv2_test_generator/reader.py
@@ -1,4 +1,5 @@
-max_val=1023
+avg_over_n=10
+max_possible=1023
 
 import sys, serial, struct, os
 if (len(sys.argv) not in [2,3]):
@@ -13,20 +14,32 @@ print("Opening %s at %d baud" % (port,baud))
 ser = serial.Serial(port,baud)
 ser.timeout = 3
 
-def disp_val(val):
+def disp_val(avg_val,min_val,max_val):
+    avg_val=int(round(avg_val))
+    min_val=int(round(min_val))
+    max_val=int(round(max_val))
+    
     size = os.get_terminal_size()
     width = size[0]
     width -= 4+1+1 # subtract used up chars
-    pad_before = int(round(width * (val/max_val)))
+    pad_before = int(round(width * (avg_val/max_possible)))
     line = ""
     line+='|'
     line+='-'*pad_before
-    line+="%04d"%(val)
+    line+="%04d"%(avg_val)
     line+='-'*(width-pad_before)
     line+='|'
     print(line)
 
 while True:
-    b = ser.read(2)
-    t = struct.unpack('<H',b)
-    disp_val(t[0])
+    min_val=max_possible
+    max_val=0
+    avg_val=0
+    for n in range(0,avg_over_n):
+        b = ser.read(2)
+        i = struct.unpack('<H',b)[0]
+        max_val = max(max_val,i)
+        min_val = min(min_val,i)
+        avg_val+=i
+    avg_val /= avg_over_n
+    disp_val(avg_val,min_val,max_val)
